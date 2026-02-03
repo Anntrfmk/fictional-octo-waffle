395d29d2b9ab6d6ad820af2b6c239d3338639431
Ann <anntrofymiuk@gmail.com>
2026-02-03 23:53:47 +0200

    first commit

---
+++ script1.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//alert("Hello");
+//const result = confirm("Are you here&");
+//console.log(result);
+
+//const answer = prompt("do you love me?", "yes");
+//console.log(typeof(answer));
+//console.log(answer+5);
+
+const answers =[];
+answers[0] = prompt("what is your name?",'');
+answers[1] = prompt("what is your lastname?","");
+answers[3] = prompt("how old are you?","");
+
+document.whrite(answers);
+
+console.log(4+ +"5");
+
+let incr=10,
+    decr=10;
+//incr++;постфікс//++incr;префікс
+//decr--;//--decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 =='8');//можна з і без ''
+console.log(2*4===8);
+ const inChecked= true;
+ isClose = false;
+
+console.log(isChecked && isClose);//porivniania tilku oba
+console.log(isChecked || isClose);//porivniania dage jaksho 1 
+
+ console.log(isChecked || !isClose);//!-true->false
+ 
+      
